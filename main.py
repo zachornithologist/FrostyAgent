afb54c5e9783d07f23ab43259d1bdef4922788cb
Zach_Dev <158592119+zachornithologist@users.noreply.github.com>
2024-05-27 16:12:31 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from train import train_agent
+from evaluate import evaluate_agent
+
+if __name__ == "__main__":
+    # Train the agent
+    train_agent()
+    
+    # Evaluate the agent
+    evaluate_agent()
